e13ff9737dd1c6a61430bb61352a183bc26cc0dc
gamanedy144 <gamanedy@yahoo.com>
2021-07-24 18:48:57 +0300

    Add multiple sizes for canvas and fixed to stay the same width and height

--- script.js
+++ script.js
@@ -2,52 +2,78 @@ const container = document.querySelector('#grid-container');
 let rows = document.getElementsByClassName('gridRow');
 let cells = document.getElementsByClassName('cell');
 let trigger = false;
-let squareDiv = document.createElement('div');
-squareDiv.classList.add('square');
+
 let squareRow = document.createElement('div');
+const resetButton = document.querySelector('#reset-button');
+const newSizeButton = document.querySelector('#new-size-button');
 
-function makeRows (rowNum){
-    for (let i = 0; i < rowNum; i++){
-        let row = document.createElement('div');
-        container.appendChild(row).className = "gridRow";
+function makeRows (number){
+    for(let i = 0; i < number * number; i++){
+        let cell = document.createElement('div');
+        //addEventToCell(cell);
+        cell.addEventListener("mouseenter", function(e){
+            e.preventDefault();
+            if(trigger == true){
+                // this.style.backgroundColor = "black";
+                this.classList.add('dark-cell');
+            };
+        });
+        container.appendChild(cell).className = 'cell';
+
     }
 }
+window.addEventListener('load', setDefaultGrid());
 
-function makeColumns (colNum){
-    for(let i = 0; i < rows.length; i++){
-        for(let j = 0; j < colNum; j++){
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className = "cell";
-            addEventToCell(newCell);
-        }
-    }
+function setGridSize(size) {
+    container.style.setProperty('--grid-rows', size);
+    container.style.setProperty('--grid-cols', size);
 }
-
-function defaultGrid(){
+function setDefaultGrid(){
+    setGridSize(16);
     makeRows(16);
-    makeColumns(16);
 }
-defaultGrid();
-
-let cellArray = document.querySelectorAll(".cell");
-cellArray.forEach(function(e){
-    e.addEventListener("mouseenter", function(e){
-        e.preventDefault();
-        if(trigger == true){
-            this.style.backgroundColor = "black";
-        }
-    })
-})
-document.addEventListener('mousedown', function(){
-    trigger = true;
-})
-document.addEventListener('mouseup', function(){
-    trigger = false;
-})
 
 
+document.addEventListener('mouseover',function(){
+    trigger=true;
+})
 function addEventToCell(cell){
     cell.addEventListener('mousedown', () =>{
-        cell.style.backgroundColor = "black";
+        // cell.style.backgroundColor = "black";
+        cell.classList.add('dark-cell');
     })
 }
+// reset Grid function
+function resetGrid(){
+    let cellArray = document.querySelectorAll(".cell");
+    cellArray.forEach(function(e){
+        e.classList.remove('dark-cell');
+    });
+    // console.log('Reset button clicked');
+}
+resetButton.addEventListener('click', resetGrid);
+
+function clearGrid() {
+    const gridArray = Array.from(container.childNodes);
+    gridArray.forEach((element) => {
+      container.removeChild(element);
+    });
+  }
+function changeSize(){
+    let newSize = prompt('Enter new size');
+    if (newSize !== null) {
+        newSize = parseInt(newSize);
+        if (newSize < 1 || newSize > 64 || Number.isNaN(newSize)) {
+          alert("Enter a number from 1-64 range");
+          changeSize();
+        } 
+        else {
+            clearGrid();
+            resetGrid();
+            setGridSize(newSize);
+            makeRows(newSize);
+        }
+    }
+}
+
+newSizeButton.addEventListener('click', changeSize);
